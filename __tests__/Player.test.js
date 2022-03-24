d3c3edbe6db68eca278cbf80d583e52e8afdf566
= <holdanmcmullen@gmail.com>
2022-03-24 13:18:41 -0600

    finished enemy object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -3,8 +3,6 @@ const Player = require("../lib/Player");
 
 jest.mock("../lib/Potion");
 
-console.log(new Potion());
-
 test("creates a player object", () => {
     const player = new Player("Dave");
 
