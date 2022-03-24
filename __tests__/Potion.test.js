901be2733a05262d17427aa6b0cc652e503e34b3
= <holdanmcmullen@gmail.com>
2022-03-24 12:30:40 -0600

    finished player object

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,3 +1,4 @@
+const Player = require("../lib/Player.js");
 const Potion = require("../lib/Potion.js");
 
 test('creates a health potion object', () => {
@@ -13,4 +14,31 @@ test('creates a random potion object', () => {
     expect(potion.name).toEqual(expect.any(String));
     expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
+});
+
+test("gets player's attack value", () => {
+    const player = new Player("Dave");
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test("adds a potion to the inventory", () => {
+    const player = new Player("Dave");
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test("uses a potion from inventory", () => {
+    const player = new Player("Dave");
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
 });
